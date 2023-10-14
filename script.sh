eaae4e640b32444da82a6c71793af6fd74b22b1b
DinryWJ <827121759@qq.com>
2023-10-14 23:57:40 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -18,10 +18,8 @@ grep -o 'Tunnel established at tcp://[^ ]*' /home/dinry/opensource/nat/nat.log |
     "url": "https://mritd.com"
   }'
 
-  # 使用jq命令来修改JSON字符串，将读取到的行写入"body"字段
   json_modified=$(echo "$json" | LINE="$line" jq '.body = env.LINE')
 
-  # 使用curl发送JSON数据到API
   curl -X POST "https://api.day.app/BARK_KEY" \
      -H 'Content-Type: application/json; charset=utf-8' \
      -d "$json_modified"
